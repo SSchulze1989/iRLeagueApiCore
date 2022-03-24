ed7e0841dd9771a2a1da97a1fcef382c7f9471dc
Simon Schulze <simonschulze2589@hotmail.de>
2022-03-24 04:51:04 +0100

    Add Admin Controller

---
+++ iRLeagueApiCore.Communication/Models/GetUserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRLeagueApiCore.Communication.Models
+{
+    public class GetAdminUserModel
+    {
+        public string UserName { get; set; }
+        public string Firsname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
---
+++ iRLeagueApiCore.Server/Controllers/AdminController.cs
@@ -0,0 +1,71 @@
+﻿using iRLeagueApiCore.Communication.Models;
+using iRLeagueApiCore.Server.Authentication;
+using iRLeagueApiCore.Server.Filters;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iRLeagueApiCore.Server.Controllers
+{
+    [ApiController]
+    [ServiceFilter(typeof(LeagueAuthorizeAttribute))]
+    [RequireLeagueRoles(LeagueRoles.Admin)]
+    [Route("{leagueName}/[controller]")]
+    public class AdminController : LeagueApiController
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(ILogger<AdminController> logger, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        /// <summary>
+        /// List the users with at least one role in the current league
+        /// </summary>
+        [HttpGet("ListUsers")]
+        public async Task<ActionResult<IEnumerable<GetAdminUserModel>>> ListUsers([FromRoute] string leagueName)
+        {
+            _logger.LogInformation("Get list of users for {LeagueName} by {UserName}", leagueName, User.Identity.Name);
+
+            // Get users that have a league role
+            var users = new List<ApplicationUser>();
+
+            foreach (var role in LeagueRoles.RolesAvailable)
+            {
+                users.AddRange(await _userManager.GetUsersInRoleAsync(role));
+            }
+
+            if (users.Count() == 0)
+            {
+                _logger.LogInformation("No users found in {LeagueName}", leagueName);
+                return NotFound();
+            }
+
+            var getUsers = users
+                .Distinct()
+                .Select(async x => new GetAdminUserModel()
+                {
+                    UserName = x.UserName,
+                    Firsname = x.FullName.Split(' ').First(),
+                    Lastname = x.FullName.Split(' ').Last(),
+                    Email = x.Email,
+                    Roles = await _userManager.GetRolesAsync(x)
+                });
+
+            _logger.LogInformation("Return {Count} user entries for {LeagueName}", getUsers.Count(), leagueName);
+
+            return Ok(getUsers);
+        }
+    }
+}
