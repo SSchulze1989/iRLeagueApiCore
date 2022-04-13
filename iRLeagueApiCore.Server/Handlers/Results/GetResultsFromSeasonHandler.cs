3150922eabadddba68a4c8ae17ea6977f14fd104
Simon Schulze <simonschulze2589@hotmail.de>
2022-04-14 00:21:32 +0200

    Implement results handler

---
+++ iRLeagueApiCore.Server/Handlers/Results/GetResultsFromSeasonHandler.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using iRLeagueApiCore.Communication.Models;
+using iRLeagueApiCore.Server.Exceptions;
+using iRLeagueDatabaseCore.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace iRLeagueApiCore.Server.Handlers.Results
+{
+    public record GetResultsRequest(long LeagueId, long SeasonId) : IRequest<IEnumerable<GetResultModel>>;
+
+    public class GetResultsFromSeasonHandler : ResultHandlerBase<GetResultsFromSeasonHandler, GetResultsRequest>, IRequestHandler<GetResultsRequest, IEnumerable<GetResultModel>>
+    {
+        public GetResultsFromSeasonHandler(ILogger<GetResultsFromSeasonHandler> logger, LeagueDbContext dbContext, IEnumerable<IValidator<GetResultsRequest>> validators) :
+            base(logger, dbContext, validators)
+        {
+        }
+
+        public async Task<IEnumerable<GetResultModel>> Handle(GetResultsRequest request, CancellationToken cancellationToken)
+        {
+            await validators.ValidateAllAndThrowAsync(request, cancellationToken);
+            var getResults = await MapToGetResultModelsFromSeasonAsync(request.LeagueId, request.SeasonId, cancellationToken);
+            if (getResults.Count() == 0)
+            {
+                throw new ResourceNotFoundException();
+            }
+            return getResults;
+        }
+
+        private async Task<IEnumerable<GetResultModel>> MapToGetResultModelsFromSeasonAsync(long leagueId, long seasonId, CancellationToken cancellationToken)
+        {
+            return await dbContext.ScoredResults
+                .Where(x => x.LeagueId == leagueId)
+                .Where(x => x.Result.Session.Schedule.SeasonId == seasonId)
+                .Select(MapToGetResultModelExpression)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
