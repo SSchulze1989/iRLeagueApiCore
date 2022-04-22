417182dc4f9d8e708808ab5806e31d87746b7121
Simon Schulze <simonschulze2589@hotmail.de>
2022-04-22 16:35:55 +0200

    Basic testing

---
+++ iRLeagueApiCore.UnitTests/Client/Endpoints/EndpointsTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity.Test;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace iRLeagueApiCore.UnitTests.Client.Endpoints
+{
+    public class EndpointsTests
+    {
+        public const string BaseUrl = "https://example.com/api/";
+
+        public static async Task TestRequestUrl<TEndpoint>(string expectedUrl, Func<HttpClient, TEndpoint> endpoint, Func<TEndpoint, Task> action)
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(null));
+            string requestUrl = "";
+            var httpMessageHandler = MockHelpers.TestMessageHandler(x =>
+            {
+                requestUrl = x.RequestUri.AbsoluteUri;
+                return new HttpResponseMessage()
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = content,
+                };
+            });
+
+            var testClient = new HttpClient(httpMessageHandler);
+            testClient.BaseAddress = new Uri(BaseUrl);
+            await action.Invoke(endpoint.Invoke(testClient));
+
+            Assert.Equal(expectedUrl, requestUrl);
+        }
+    }
+}
