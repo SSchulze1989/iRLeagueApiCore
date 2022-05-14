7905dc382d7f99ba9c5d8bf4055a62952f27789f
Simon Schulze <simonschulze2589@hotmail.de>
2022-05-14 12:31:16 +0200

    Add endpoint for all sessions in season

---
+++ iRLeagueApiCore.Server/Handlers/Sessions/GetSessionsFromSeasonHandler.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using iRLeagueApiCore.Communication.Models;
+using iRLeagueDatabaseCore.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace iRLeagueApiCore.Server.Handlers.Sessions
+{
+    public record GetSessionsFromSeasonRequest(long LeagueId, long SeasonId) : IRequest<IEnumerable<GetSessionModel>>;
+
+    public class GetSessionsFromSeasonHandler : SessionHandlerBase<GetSessionsFromSeasonHandler, GetSessionsFromSeasonRequest>,
+        IRequestHandler<GetSessionsFromSeasonRequest, IEnumerable<GetSessionModel>>
+    {
+        public GetSessionsFromSeasonHandler(ILogger<GetSessionsFromSeasonHandler> logger, LeagueDbContext dbContext, IEnumerable<IValidator<GetSessionsFromSeasonRequest>> validators) : 
+            base(logger, dbContext, validators)
+        {
+        }
+
+        public async Task<IEnumerable<GetSessionModel>> Handle(GetSessionsFromSeasonRequest request, CancellationToken cancellationToken)
+        {
+            await validators.ValidateAllAndThrowAsync(request, cancellationToken);
+            var getSessions = await MapToGetSessionModelsAsync(request.LeagueId, request.SeasonId, cancellationToken);
+            return getSessions;
+        }
+
+        private async Task<IEnumerable<GetSessionModel>> MapToGetSessionModelsAsync(long leagueId, long sessionId, CancellationToken cancellationToken)
+        {
+            return await dbContext.Sessions
+                .Where(x => x.LeagueId == leagueId)
+                .Where(x => x.SessionId == sessionId)
+                .Select(MapToGetSessionModelExpression)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
