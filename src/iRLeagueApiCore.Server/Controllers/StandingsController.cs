3edabfa4408944958dbf5a30e0a225bb6a704662
Simon Schulze <simonschulze2589@hotmail.de>
2022-10-27 19:02:12 +0200

    Add standings handlers and controller

---
+++ src/iRLeagueApiCore.Server/Controllers/StandingsController.cs
@@ -0,0 +1,41 @@
+﻿using iRLeagueApiCore.Common.Models.Standings;
+using iRLeagueApiCore.Server.Filters;
+using iRLeagueApiCore.Server.Handlers.Standings;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace iRLeagueApiCore.Server.Controllers
+{
+    [Route("/{leagueName}/[controller]")]
+    [TypeFilter(typeof(LeagueAuthorizeAttribute))]
+    [TypeFilter(typeof(InsertLeagueIdAttribute))]
+    [RequireLeagueRole]
+    public class StandingsController : LeagueApiController<StandingsController>
+    {
+        public StandingsController(ILogger<StandingsController> logger, IMediator mediator) : 
+            base(logger, mediator)
+        {
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("{seasonId:long}")]
+        public async Task<ActionResult<IEnumerable<StandingsModel>>> GetFromSeason([FromRoute] string leagueName, [FromFilter] long leagueId, [FromRoute] long seasonId,
+            CancellationToken cancellationToken = default)
+        {
+            _logger.LogInformation("[{Method}] all standings from season {SeasonId} in {LeagueName} by {UserName}",
+                "Get", seasonId, leagueName, GetUsername());
+            var request = new GetStandingsFromSeasonRequest(leagueId, seasonId);
+            var getStandings = await mediator.Send(request, cancellationToken);
+            _logger.LogInformation("Return {Count} entries for standings from season {SeasonId} in {LeagueName}",
+                getStandings.Count(), seasonId, leagueName);
+            return Ok(getStandings);
+        }
+    }
+}
