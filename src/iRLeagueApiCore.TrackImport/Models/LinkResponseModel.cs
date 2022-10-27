64fb2f034aec794d2724fc3a0c0e5e1f200f7d59
Simon Schulze <simonschulze2589@hotmail.de>
2022-10-27 12:30:29 +0200

    Update tests

---
+++ src/iRLeagueApiCore.TrackImport/Models/LinkResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRLeagueApiCore.TrackImport.Models
+{
+    public struct LinkResponseModel
+    {
+        public string link { get; set; }
+        public DateTime expires { get; set; }
+    }
+}
--- test/iRLeagueApiCore.UnitTests/TrackImport/TrackImportServiceTests.cs
+++ test/iRLeagueApiCore.UnitTests/TrackImport/TrackImportServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using iRLeagueApiCore.TrackImport.Service;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -14,16 +15,87 @@ namespace iRLeagueApiCore.UnitTests.TrackImport
 {
     public class TrackImportServiceTests
     {
+        private readonly IConfiguration configuration;
+        private const string Skip = "Skip tests that hit external api";
+
+        private const string testUserName = "CLunky@iracing.Com";
+        private const string testPassword = "MyPassWord";
+        private const string testPasswordEncoded = "xGKecAR27ALXNuMLsGaG0v5Q9pSs2tZTZRKNgmHMg+Q=";
+
+        public TrackImportServiceTests()
+        {
+            configuration = ((IConfigurationBuilder)(new ConfigurationBuilder()))
+                .AddUserSecrets<TrackImportServiceTests>()
+                .Build();
+        }
         [Fact]
         public void EncodePassword_ShouldEncodeTestHash_WhenUsingTestPassword()
         {
-            string userName = "CLunky@iracing.Com";
-            string password = "MyPassWord";
-            string expected = "xGKecAR27ALXNuMLsGaG0v5Q9pSs2tZTZRKNgmHMg+Q=";
+            string userName = testUserName;
+            string password = testPassword;
+            string expected = testPasswordEncoded;
 
             var encoded = TrackImportService.EncodePassword(userName, password);
 
             encoded.Should().Be(expected);
+        }
+
+        [Fact(Skip = Skip)]
+        public async Task Authenticate_ShouldReturnTrue_WhenUsingValidCredentials()
+        {
+            string username = configuration["Iracing:UserName"];
+            string password = configuration["Iracing:Password"];
+            var cookieContainer = new CookieContainer();
+            var httpClientHander = new HttpClientHandler()
+            {
+                UseCookies = true,
+                CookieContainer = cookieContainer
+            };
+
+            var sut = new TrackImportService(new(httpClientHander));
+            var result = await sut.Authenticate(username, password);
+
+            result.Should().BeTrue();
+            cookieContainer.GetAllCookies().Should().NotBeEmpty();
+        }
+
+        [Fact(Skip = Skip)]
+        public async Task Authenticate_ShouldReturnFalse_WhenUsingInvalidCredentials()
+        {
+            var username = testUserName;
+            var password = testPassword;
+            var cookieContainer = new CookieContainer();
+            var httpClientHander = new HttpClientHandler()
+            {
+                UseCookies = true,
+                CookieContainer = cookieContainer
+            };
+
+            var sut = new TrackImportService(new(httpClientHander));
+            var result = await sut.Authenticate(username, password);
+
+            result.Should().BeFalse();
+        }
+
+        [Fact(Skip = Skip)]
+        public async Task GetTracksData_ShouldReturnData_WhenUsingValidCredentials()
+        {
+            string username = configuration["Iracing:UserName"];
+            string password = configuration["Iracing:Password"];
+            var cookieContainer = new CookieContainer();
+            var httpClientHander = new HttpClientHandler()
+            {
+                UseCookies = true,
+                CookieContainer = cookieContainer
+            };
+
+            var sut = new TrackImportService(new(httpClientHander));
+            await sut.Authenticate(username, password);
+            cookieContainer.GetCookies(new("https://members-ng.iracing.com")).Should().NotBeEmpty();
+            var result = await sut.GetTracksData();
+
+            result.Should().NotBeNull();
+            result.Should().NotBeEmpty();
         }
     }
 }
