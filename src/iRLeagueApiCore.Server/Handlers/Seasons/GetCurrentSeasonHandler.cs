a3d144ee7138183421b9543f2fbda4368d990f00
Simon Schulze <simonschulze2589@hotmail.de>
2022-11-22 23:20:03 +0100

    Get current season endpoint

---
+++ src/iRLeagueApiCore.Server/Handlers/Seasons/GetCurrentSeasonHandler.cs
@@ -0,0 +1,29 @@
+﻿using iRLeagueApiCore.Common.Models;
+
+namespace iRLeagueApiCore.Server.Handlers.Seasons
+{
+    public record GetCurrentSeasonRequest(long LeagueId) : IRequest<SeasonModel>;
+
+    public class GetCurrentSeasonHandler : SeasonHandlerBase<GetCurrentSeasonHandler, GetCurrentSeasonRequest>,
+        IRequestHandler<GetCurrentSeasonRequest, SeasonModel>
+    {
+        public GetCurrentSeasonHandler(ILogger<GetCurrentSeasonHandler> logger, LeagueDbContext dbContext, IEnumerable<IValidator<GetCurrentSeasonRequest>> validators) : 
+            base(logger, dbContext, validators)
+        {
+        }
+
+        public async Task<SeasonModel> Handle(GetCurrentSeasonRequest request, CancellationToken cancellationToken)
+        {
+            await validators.ValidateAllAndThrowAsync(request, cancellationToken);
+            var getSeasonId = await dbContext.Events
+                .Where(x => x.Date < DateTime.UtcNow)
+                .OrderBy(x => x.Date)
+                .Select(x => (long?)x.Schedule.SeasonId)
+                .FirstOrDefaultAsync(cancellationToken)
+                ?? throw new ResourceNotFoundException();
+            var getSeason = await MapToGetSeasonModel(request.LeagueId, getSeasonId, cancellationToken)
+                ?? throw new ResourceNotFoundException();
+            return getSeason;
+        }
+    }
+}
