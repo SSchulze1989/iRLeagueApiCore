4e79bb43b8db8ad26812e0713fe338e99f3325f7
Simon Schulze <simonschulze2589@hotmail.de>
2023-02-23 23:37:13 +0100

    Remove request host from logs

--- src/iRLeagueApiCore.Server/Startup.cs
+++ src/iRLeagueApiCore.Server/Startup.cs
@@ -209,7 +209,7 @@ public sealed class Startup
         app.UseSerilogRequestLogging(options =>
         {
             // Customize the message template
-            options.MessageTemplate = "{RemoteIpAddress} {RequestScheme} {RequestHost} {UserName} {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
+            options.MessageTemplate = "{RemoteIpAddress} {RequestScheme} {UserName} {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
 
             // Emit debug-level events instead of the defaults
             options.GetLevel = (httpContext, elapsed, ex) => LogEventLevel.Information;
@@ -217,7 +217,6 @@ public sealed class Startup
             // Attach additional properties to the request completion event
             options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
             {
-                diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
                 diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
                 diagnosticContext.Set("RemoteIpAddress", httpContext.Connection.RemoteIpAddress);
                 diagnosticContext.Set("UserName", httpContext.User.Identity?.Name ?? "");
