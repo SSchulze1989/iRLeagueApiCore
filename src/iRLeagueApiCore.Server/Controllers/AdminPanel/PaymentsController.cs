6fd6cb05fc743212c96d32616842e375458d9cb8
Simon Schulze <simonschulze2589@hotmail.de>
2023-07-03 23:10:33 +0200

    Add handling for setting subscription status manually

--- src/iRLeagueApiCore.Server/Controllers/AdminPanel/PaymentsController.cs
+++ src/iRLeagueApiCore.Server/Controllers/AdminPanel/PaymentsController.cs
@@ -1,4 +1,5 @@
-﻿using iRLeagueApiCore.Server.Filters;
+﻿using iRLeagueApiCore.Common.Models;
+using iRLeagueApiCore.Server.Filters;
 using iRLeagueApiCore.Server.Handlers.AdminPanel;
 using iRLeagueApiCore.Server.Models.Payments;
 using Microsoft.AspNetCore.Authorization;
@@ -8,7 +9,7 @@ namespace iRLeagueApiCore.Server.Controllers.AdminPanel;
 
 [ApiController]
 [TypeFilter(typeof(DefaultExceptionFilterAttribute))]
-[Authorize]
+[Authorize(Roles = "Admin")]
 [Route("AdminPanel/[controller]")]
 public class PaymentsController : ControllerBase
 {
@@ -33,7 +34,13 @@ public class PaymentsController : ControllerBase
         return Ok(result);
     }
 
-    [HttpGet("id:Guid")]
+    /// <summary>
+    /// Get single payment by id
+    /// </summary>
+    /// <param name="id">Id of the payment</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    [HttpGet("{id:Guid}")]
     public async Task<ActionResult<PaymentModel>> GetById([FromRoute] Guid id, CancellationToken cancellationToken)
     {
         var request = new GetPaymentRequest(id);
@@ -41,6 +48,13 @@ public class PaymentsController : ControllerBase
         return Ok(result);
     }
 
+    /// <summary>
+    /// Create a new payment 
+    /// </summary>
+    /// <param name="leagueId">Id of league for which the payment will be set</param>
+    /// <param name="model">Payment data</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     [HttpPost]
     public async Task<ActionResult<PaymentModel>> Post([FromQuery] long leagueId, [FromBody] PostPaymentModel model, CancellationToken cancellationToken)
     {
@@ -49,10 +63,32 @@ public class PaymentsController : ControllerBase
         return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
     }
 
-    [HttpPost("id:Guid/Deactivate")]
+    /// <summary>
+    /// Deactivate a running payment (when Subscription is cancelled)
+    /// </summary>
+    /// <param name="id">Id of the payment</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    [HttpPost("{id:Guid}/Deactivate")]
     public async Task<ActionResult<PaymentModel>> Deactivate([FromRoute] Guid id, CancellationToken cancellationToken)
     {
         var request = new DeactivatePaymentRequest(id);
+        var result = await mediator.Send(request, cancellationToken);
+        return Ok(result);
+    }
+
+    /// <summary>
+    /// Directly set the current league subscription status
+    /// </summary>
+    /// <param name="leagueId">Id of the league</param>
+    /// <param name="model">Status and expiration of subscription</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    [HttpPost("SetSubscription/{leagueId:long}")]
+    public async Task<ActionResult<LeagueModel>> SetSubscription([FromRoute] long leagueId, [FromBody] SetLeagueSubscriptionModel model,
+        CancellationToken cancellationToken)
+    {
+        var request = new SetLeagueSubscriptionRequest(leagueId, model);
         var result = await mediator.Send(request, cancellationToken);
         return Ok(result);
     }
