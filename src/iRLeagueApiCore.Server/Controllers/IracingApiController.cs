cc55a4a62af09a733f5d2ede8f2c76d2e028c6f9
Simon Schulze <simonschulze2589@hotmail.de>
2024-04-21 23:21:25 +0200

    Add method to test league connection

--- src/iRLeagueApiCore.Server/Controllers/IracingApiController.cs
+++ src/iRLeagueApiCore.Server/Controllers/IracingApiController.cs
@@ -1,6 +1,8 @@
-﻿
+﻿using Aydsko.iRacingData;
+using Aydsko.iRacingData.Exceptions;
 using iRLeagueApiCore.Common.Models;
 using iRLeagueApiCore.Common.Models.Leagues;
+using iRLeagueApiCore.Common.Responses;
 using iRLeagueApiCore.Server.Handlers.Leagues;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,7 +18,7 @@ public class IracingApiController : LeagueApiController<IracingApiController>
 
     [HttpPost]
     [Authorize]
-    [Route("SearchLeagues")]
+    [Route("Leagues/Searchs")]
     public async Task<ActionResult<PaginatedResultModel<IEnumerable<SearchIracingLeagueResultModel>>>> SearchLeagues([FromBody] PostSearchIracingLeaguesModel searchModel,
         [FromQuery] int page = 1,
         [FromQuery] int pageSize = 40,
@@ -29,5 +31,45 @@ public class IracingApiController : LeagueApiController<IracingApiController>
         var request = new SearchIracingLeaguesRequest(searchModel.Search, page, pageSize);
         var result = await mediator.Send(request, cancellationToken);
         return Ok(result);
+    }
+
+    /// <summary>
+    /// Test if 
+    /// </summary>
+    /// <param name="iracingLeagueId"></param>
+    /// <param name="cancellationToken"></param>
+    /// <param name="dataClient"></param>
+    /// <returns></returns>
+    [HttpPost]
+    [Authorize]
+    [Route("League/{iracingLeagueId:int}/Test")]
+    public async Task<ActionResult<StatusResponse>> TestLeagueConnection([FromRoute] int iracingLeagueId, [FromServices] IDataClient dataClient, 
+        CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var result = await dataClient.GetLeagueAsync(iracingLeagueId, cancellationToken: cancellationToken);
+        } 
+        catch (HttpRequestException)
+        {
+            return Ok(new StatusResponse()
+            {
+                Status = "Not Found",
+                Message = $"No league found with id {iracingLeagueId}",
+            });
+        }
+        catch (iRacingForbiddenResponseException)
+        {
+            return Ok(new StatusResponse()
+            {
+                Status = "Access Denied",
+                Message = "Cannot access private league data",
+            });
+        }
+        return Ok(new StatusResponse()
+        {
+            Status = "Success",
+            Message = "League connection established successfully",
+        });
     }
 }
