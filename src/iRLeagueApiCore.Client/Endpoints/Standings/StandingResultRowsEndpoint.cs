4cda60545377c83193ef3b773667c6e18edcba59
Simon Schulze <simonschulze2589@hotmail.de>
2024-07-11 00:30:03 +0200

    Add client endpoint for override dropweeks

---
+++ src/iRLeagueApiCore.Client/Endpoints/Standings/StandingResultRowsEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using iRLeagueApiCore.Client.Http;
+using iRLeagueApiCore.Client.QueryBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRLeagueApiCore.Client.Endpoints.Standings;
+internal sealed class StandingResultRowsEndpoint : EndpointBase, IWithIdEndpoint<IStandingResultRowByIdEndpoint>
+{
+    public StandingResultRowsEndpoint(HttpClientWrapper httpClient, RouteBuilder routeBuilder) 
+        : base(httpClient, routeBuilder)
+    {
+        RouteBuilder.AddEndpoint("ResultRows");
+    }
+
+    public IStandingResultRowByIdEndpoint WithId(long id)
+    {
+        return new StandingResultRowByIdEndpoint(HttpClientWrapper, RouteBuilder, id);
+    }
+}
