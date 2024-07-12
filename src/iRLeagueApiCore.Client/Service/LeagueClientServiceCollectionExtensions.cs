931be288189be38140533e32672ee8707baef202
Simon Schulze <simonschulze2589@hotmail.de>
2024-07-12 12:57:27 +0200

    Add default jsonOptions during service intialization

--- src/iRLeagueApiCore.Client/Service/LeagueClientServiceCollectionExtensions.cs
+++ src/iRLeagueApiCore.Client/Service/LeagueClientServiceCollectionExtensions.cs
@@ -3,6 +3,8 @@ using iRLeagueApiCore.Client.Http;
 using iRLeagueApiCore.Client.Service;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 namespace Microsoft.Extensions.DependencyInjection;
 public static class IRLeagueApiClientServiceExtensions
@@ -10,10 +12,17 @@ public static class IRLeagueApiClientServiceExtensions
     public static IServiceCollection AddLeagueApiClient(this IServiceCollection services, Action<LeagueApiClientConfiguration>? configure = null)
     {
         services.AddHttpClient();
-        services.TryAddScoped<HttpClientWrapperFactory>();
+
+        var jsonOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+        jsonOptions.Converters.Add(new JsonStringEnumConverter());
+        jsonOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+
+        services.TryAddScoped<HttpClientWrapperFactory>(sp => new(
+                sp.GetRequiredService<ILoggerFactory>(),
+                jsonOptions
+            ));
         services.TryAddScoped<ITokenStore, DefaultTokenStore>();
         services.TryAddScoped<IAsyncTokenProvider>(sp => sp.GetRequiredService<ITokenStore>());
-
         // run configuration
         var configuration = new LeagueApiClientConfiguration(services);
         configure?.Invoke(configuration);
