b182b9c259c51299bee2d77af7075da54cdb26ec
Simon Schulze <simonschulze2589@hotmail.de>
2024-09-20 23:50:53 +0200

    Add handling for min & max comparison in filters

--- src/iRLeagueApiCore.Common/Models/ResultConfigurations/BonusPointModel.cs
+++ src/iRLeagueApiCore.Common/Models/ResultConfigurations/BonusPointModel.cs
@@ -11,4 +11,9 @@ public class BonusPointModel
     public double Points { get; set; }
     [DataMember]
     public ICollection<FilterConditionModel> Conditions { get; set; } = new List<FilterConditionModel>();
+    /// <summary>
+    /// Maximum number of drivers/teams that are allowed to receive this bonus - if the conditions apply to more than this number the bonus will not be applied
+    /// </summary>
+    [DataMember]
+    public int MaxCount { get; set; }
 }
