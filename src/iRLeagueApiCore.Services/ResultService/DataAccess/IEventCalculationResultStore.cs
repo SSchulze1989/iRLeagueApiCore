1b31ca35fea8e6fddca8e481ad733b6c61d012e8
Simon Schulze <57634354+SSchulze1989@users.noreply.github.com>
2024-12-31 13:16:51 +0100

    [F] Prune results that are not configured for the selected event (#213)
    Prune results that are not configured for the selected event

--- src/iRLeagueApiCore.Services/ResultService/DataAccess/IEventCalculationResultStore.cs
+++ src/iRLeagueApiCore.Services/ResultService/DataAccess/IEventCalculationResultStore.cs
@@ -5,4 +5,11 @@ namespace iRLeagueApiCore.Services.ResultService.DataAccess;
 internal interface IEventCalculationResultStore
 {
     public Task StoreCalculationResult(EventCalculationResult result, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Remove results that are not in the listed resultConfigIds
+    /// </summary>
+    /// <param name="resultConfigIds"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public Task<IEnumerable<long>> PruneResults(long eventId, IEnumerable<long?> resultConfigIds, CancellationToken cancellationToken = default);
 }
