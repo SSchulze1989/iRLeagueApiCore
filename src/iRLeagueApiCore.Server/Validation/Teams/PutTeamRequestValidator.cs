4bcb3ccd38730e5190a1da879be1dab63001a73f
Simon Schulze <simonschulze2589@hotmail.de>
2025-02-23 15:12:37 +0100

    Enable archiving Teams.

---
+++ src/iRLeagueApiCore.Server/Validation/Teams/PutTeamRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using iRLeagueApiCore.Server.Handlers.Teams;
+
+namespace iRLeagueApiCore.Server.Validation.Teams;
+
+public class PutTeamRequestValidator : AbstractValidator<PutTeamRequest>
+{
+    public PutTeamRequestValidator()
+    {
+        RuleFor(x => x.Model.Members)
+            .Empty()
+            .When(x => x.Model.IsArchived)
+            .WithMessage("Members list must be empty when Team is archived (IsArchived == true)");
+    }
+}
