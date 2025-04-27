b781ee6c6ab732590bac576035e0f694c52176a4
Simon Schulze <simonschulze2589@hotmail.de>
2025-04-27 13:16:09 +0200

    [F] Add DTOs for Roster management

---
+++ src/iRLeagueApiCore.Common/Models/Rosters/RosterInfoModel.cs
@@ -0,0 +1,18 @@
+﻿namespace iRLeagueApiCore.Common.Models.Rosters;
+
+[DataContract]
+public class RosterInfoModel
+{
+    [DataMember]
+    public long RosterId { get; set; }
+    /// <summary>
+    /// Name of the roster
+    /// </summary>
+    [DataMember]
+    public string Name { get; set; } = string.Empty;
+    /// <summary>
+    /// [Optional] Description of the roster
+    /// </summary>
+    [DataMember]
+    public string Description { get; set; } = string.Empty;
+}
