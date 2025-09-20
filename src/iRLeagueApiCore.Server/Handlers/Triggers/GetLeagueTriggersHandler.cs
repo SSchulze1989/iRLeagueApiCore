57af45453118bfd4fc00aa90f1d79b51a945ec55
Simon Schulze <simonschulze2589@hotmail.de>
2025-09-20 02:19:08 +0200

    [F] Add Trigger API handlers

---
+++ src/iRLeagueApiCore.Server/Handlers/Triggers/GetLeagueTriggersHandler.cs
@@ -0,0 +1,22 @@
+﻿using iRLeagueApiCore.Common.Models;
+
+namespace iRLeagueApiCore.Server.Handlers.Triggers;
+
+public record GetTriggersRequest(long LeagueId) : IRequest<IEnumerable<TriggerModel>>;
+
+public class GetLeagueTriggersHandler : TriggersHandlerBase<GetLeagueTriggersHandler, GetTriggersRequest, IEnumerable<TriggerModel>>
+{
+    public GetLeagueTriggersHandler(ILogger<GetLeagueTriggersHandler> logger, LeagueDbContext dbContext, IEnumerable<IValidator<GetTriggersRequest>> validators) :
+        base(logger, dbContext, validators)
+    {
+    }
+
+    public override async Task<IEnumerable<TriggerModel>> Handle(GetTriggersRequest request, CancellationToken cancellationToken)
+    {
+        await validators.ValidateAllAndThrowAsync(request, cancellationToken);
+        var triggers = await dbContext.Triggers
+            .Select(MapToTriggerExpression)
+            .ToListAsync(cancellationToken);
+        return triggers;
+    }
+}
---
+++ src/iRLeagueApiCore.Server/Handlers/Triggers/TriggersHandlerBase.cs
@@ -0,0 +1,35 @@
+﻿using iRLeagueApiCore.Common.Models;
+using iRLeagueApiCore.Server.Models;
+using System.Linq.Expressions;
+
+namespace iRLeagueApiCore.Server.Handlers.Triggers;
+
+public abstract class TriggersHandlerBase<THandler, TRequest, TResponse> : HandlerBase<THandler, TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    public TriggersHandlerBase(ILogger<THandler> logger, LeagueDbContext dbContext, IEnumerable<IValidator<TRequest>> validators) :
+        base(logger, dbContext, validators)
+    {
+    }
+
+    protected TriggerEntity MapToTriggerEntity(TriggerEntity entity, PostTriggerModel model)
+    {
+        entity.Name = model.Name;
+        entity.Description = model.Description;
+        entity.TriggerType = model.TriggerType;
+        entity.Parameters = model.Parameters != null ? new Dictionary<string, object>(model.Parameters) : new Dictionary<string, object>();
+        entity.Action = model.Action;
+        entity.ActionParameters = model.ActionParameters != null ? new Dictionary<string, object>(model.ActionParameters) : new Dictionary<string, object>();
+        return entity;
+    }
+
+    protected Expression<Func<TriggerEntity, TriggerModel>> MapToTriggerExpression => trigger => new TriggerModel()
+    {
+        TriggerId = trigger.TriggerId,
+        Name = trigger.Name,
+        Description = trigger.Description,
+        TriggerType = trigger.TriggerType,
+        Parameters = trigger.Parameters,
+        Action = trigger.Action,
+        ActionParameters = trigger.ActionParameters,
+    };
+}
