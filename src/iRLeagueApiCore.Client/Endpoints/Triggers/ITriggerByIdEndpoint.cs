fc0a70421cfe78011c49d2c4a37f278d3dfd9638
Simon Schulze <simonschulze2589@hotmail.de>
2025-10-11 00:24:55 +0200

    [F] Add trigger endpoint in Client

---
+++ src/iRLeagueApiCore.Client/Endpoints/Triggers/ITriggerByIdEndpoint.cs
@@ -0,0 +1,9 @@
+﻿using iRLeagueApiCore.Client.Results;
+using iRLeagueApiCore.Common.Models;
+
+namespace iRLeagueApiCore.Client.Endpoints.Triggers;
+
+public interface ITriggerByIdEndpoint : IUpdateEndpoint<TriggerModel, PutTriggerModel>
+{
+    public IPostEndpoint<NoContent> RunTrigger();
+}
