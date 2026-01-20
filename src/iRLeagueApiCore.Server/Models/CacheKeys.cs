a2117d84032cc103a4e4fc9a4d12ab8ff4bf5933
Simon Schulze <simonschulze2589@hotmail.de>
2026-01-20 23:33:18 +0100

    [M] Add caching for the iracing password limited token
    * Bump version to 0.17.8

--- src/iRLeagueApiCore.Server/Models/CacheKeys.cs
+++ src/iRLeagueApiCore.Server/Models/CacheKeys.cs
@@ -3,4 +3,5 @@
 internal static class CacheKeys
 {
     public static string GetLeagueNameKey(string leagueName) => $"leagueName_{leagueName}";
+    public static string IracingOauthTokenKey => "iracing_oauth_token";
 }
