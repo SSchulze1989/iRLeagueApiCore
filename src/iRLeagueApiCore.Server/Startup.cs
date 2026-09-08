31968fea5e0ce04acb24f29e54da6c9f2ac5e065
Simon Schulze <simonschulze2589@hotmail.de>
2026-09-08 21:21:25 +0200

    [B] Fix broken build

--- src/iRLeagueApiCore.Server/Startup.cs
+++ src/iRLeagueApiCore.Server/Startup.cs
@@ -254,7 +254,7 @@ public sealed class Startup
         app.UseSerilogRequestLogging(options =>
         {
             // Customize the message template
-            options.MessageTemplate = "{RemoteIpAddress:l} {RequestScheme:l} {RequestMethod:l} {RequestPath:l} responded {StatusCode} in {Elapsed:0.0000} ms {RequestReferer} {RequestAgent} {UserN[...]
+            options.MessageTemplate = "{RemoteIpAddress:l} {RequestScheme:l} {RequestMethod:l} {RequestPath:l} responded {StatusCode} in {Elapsed:0.0000} ms {RequestReferer} {RequestAgent} {UserName}";
 
             // Emit debug-level events instead of the defaults
             options.GetLevel = (httpContext, elapsed, ex) => LogEventLevel.Information;
